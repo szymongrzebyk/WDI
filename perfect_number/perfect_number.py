ed0c56e6613a9982ae35f2505647f68cc2a21fd6
Szymon Grzebyk <szymongrzebyk@gmail.com>
2021-11-08 22:57:05 +0100

    Finished perfect_number

--- perfect_number/perfect_number.py
+++ perfect_number/perfect_number.py
@@ -1,19 +1,23 @@
 def dzielnik(number, start, end, suma):
-    for i in range(start, end):
-        if number % i == 0:
-            # print(i)
-            suma += i
-            if i != (number / i):
-                suma += int(number / i)
-            if not (i - (number/i) == 0 or (number/i) - i == 1):
-                # print("Kolejne wywołanie.")
-                suma = dzielnik(number, i + 1, int(number / (i + 1)), suma)
-                break
+    if number >= suma:
+        for index in range(start, end):
+            if number % index == 0:
+                # print(i)
+                suma += index
+                if index != (number / index):
+                    suma += int(number / index)
+                if not (index - (number / index) == 0 or (number / index) - index == 1):
+                    # print("Kolejne wywołanie.")
+                    suma = dzielnik(number, index + 1, int(number / (index + 1)), suma)
+                    break
     return suma
 
 
-liczba = int(input("Wpisz liczbę, którą chcesz sprawdzić: "))
+# liczba = int(input("Wpisz liczbę, którą chcesz sprawdzić: "))
 wynik = 1
 
-if liczba == dzielnik(liczba, 2, int(liczba/2), wynik):
-    print(liczba)
+for i in range(2, 1000000, 2):
+    # if i % 10000 == 0:
+        # print("Sprawdzam", i)
+    if i == dzielnik(i, 2, int(i/2), wynik):
+        print(i)
